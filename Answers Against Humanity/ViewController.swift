848dedb3a5b5dfdb8e100cf611bbcdeec4f882fd
Chris Ward <chriswhward@gmail.com>
2014-12-14 11:14:04 +0100

    Initial Commit

---
+++ Answers Against Humanity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Answers Against Humanity
+//
+//  Created by Chris Ward on 14/12/2014.
+//  Copyright (c) 2014 Gregarious Mammal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
