cb50ab7da0194c0a318086f24058e3f5ccbc6385
Chris Ward <chriswhward@gmail.com>
2014-12-23 14:04:07 +0100

    Initial Commit

--- Answers Against Humanity/ViewController.swift
+++ Answers Against Humanity/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
 
+		answerLabel.text = genText()
+		
+	}
+	
+	@IBAction func genMore(sender: AnyObject) {
+		answerLabel.text = genText()
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	@IBOutlet weak var answerLabel: UILabel!
+	
+	func genText() -> String {
+		var randString: NSString = ""
+		
+		let filePath = NSBundle.mainBundle().pathForResource("cards",ofType:"json")
+		
+		var readError:NSError?
+		if let data = NSData(contentsOfFile:filePath!,
+			options: NSDataReadingOptions.DataReadingUncached,
+			error:&readError) {
+				
+				let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments | NSJSONReadingOptions.MutableContainers,error:&readError)
+				
+				if let cardsFeed = parsedObject as? NSDictionary {
+					if let cardsArray = cardsFeed["masterCards"] as? NSArray {
+						let index: Int = Int(arc4random_uniform(UInt32(cardsArray.count)))
+						randString = cardsArray[index]["text"] as NSString
+						
+					}
+				}
+		}
+		return randString
+	}
+	
 }
 
